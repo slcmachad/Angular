bf3aa669d332b70048000bdbb55c3f8621fe2f6a
Salomao <salomao.machado.75@gmail.com>
2023-05-23 10:21:13 -0300

    Quarta parte do cuso inicial de angular. Aprendendo sobre diretivas no angular.

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -7,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
+  listaPensamentos = [
+    {
+      conteudo: 'teste',
+      autoria: 'someone',
+      modelo: 'modelo3'
+    },
+    {
+      conteudo: 'teste 2',
+      autoria: 'someone else',
+      modelo: 'modelo2'
+    },
+    {
+      conteudo: 'Mussum Ipsum, cacilds vidis litro abertis. Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi. Diuretics paradis num copo é motivis de denguis. Atirei o pau no gatis, per gatis num morreus. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis. Sapien in monti palavris qui num significa nadis i pareci latim.',
+      autoria: 'someone else',
+      modelo: 'modelo2'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
